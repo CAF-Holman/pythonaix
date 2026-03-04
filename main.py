d9ce05a534d97abf039fb62573bba19aad76f7dc
cafholman <cafholman@proton.me>
2026-03-04 10:07:12 -0500

    CH1-L3: First Hardcoded call and response

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+import os
+
+# To Import API Key
+from dotenv import load_dotenv
+
+# Importing Google Gemini AI
+from google import genai
+
+# To import API Key from .env file
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("Missing GEMINI_API_KEY in .env file")
+
+# Create New Instance of Gemini Client
+client = genai.Client(api_key=api_key)
+
+# First attempt at calling AI. Hard Coded request.
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)  # Print response from AI
+
+
+# Main Section of Code
 def main():
     print("Hello from pythonai!")
-
 
 if __name__ == "__main__":
     main()
