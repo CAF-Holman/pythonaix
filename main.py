f0a3c70238491a2206fc7cb850735681c5bcb98c
cafholman <cafholman@proton.me>
2026-03-06 08:40:49 -0500

    CH1:L6 - Starting to add multiple messages to AI

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 # Importing Google Gemini AI
 from google import genai
+from google.genai import types
 
 # To import API Key from .env file
 load_dotenv()
@@ -18,12 +19,15 @@ parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args() # Now we can access `args.user_prompt`
 
+# List for user prompts
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 # Create New Instance of Gemini Client
 client = genai.Client(api_key=api_key)
 
 # Calling Geminin Client
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt
+    model='gemini-2.5-flash', contents=messages
 )
 
 # Get metadata of response
